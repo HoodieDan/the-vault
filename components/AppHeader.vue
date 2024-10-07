91aaaa8d2d2a74161f84ea8e6cd13cebfc5d818e
hoodiedan <obodedaniel3@gmail.com>
2024-10-07 03:55:06 +0100

    implemented side nav functionality for mobile

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav>
+        <nav :class="navIsOpen ? 'opened': ''">
             <div class="container-lg container-fluid d-flex justify-content-between">
                 <div class="left__nav">
                     <img src="../assets/images/logo.png" class="nav__logo" alt="logo">
@@ -19,15 +19,25 @@
                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                     </svg>
                 </div>
-                <div class="menu">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <div class="menu" @click.prevent="toggleNav">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" v-if="!navIsOpen">
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" v-else>
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                     </svg>
                 </div>
             </div>
         </nav>
+        <aside :class="navIsOpen ? 'show' : ''">
+
+        </aside>
     </div>
 </template>
 <script setup>
+const navIsOpen = ref(false);
 
+const toggleNav = () => {
+    navIsOpen.value = !navIsOpen.value
+}
 </script>
