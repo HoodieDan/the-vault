334315e6d045874ddf9998ca35cc119123ede710
hoodiedan <obodedaniel3@gmail.com>
2024-10-10 05:05:17 +0100

    implemented product page

---
+++ pages/product/[id].vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="product__id">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 left">
+                <div class="row">
+                    <div class="col-12">
+                        <img src="../../assets/images/product-page-image-1.png" class="img-fluid" alt="product image">
+                    </div>
+                    <div class="col-3 bottom__image">
+                        <img src="../../assets/images/product-page-image-2.png" alt="product image" class="img-fluid">
+                    </div>
+                    <div class="col-3 bottom__image">
+                        <img src="../../assets/images/product-page-image-3.png" alt="product image" class="img-fluid">
+                    </div>
+                    <div class="col-3 bottom__image">
+                        <img src="../../assets/images/product-page-image-4.png" alt="product image" class="img-fluid">
+                    </div>
+                    <div class="col-3 bottom__image">
+                        <img src="../../assets/images/product-page-image-5.png" alt="product image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 right d-flex flex-column justify-content-between">
+                <div>
+                    <h5>MENS STREETWEAR BOX</h5>
+                    <h6 class="desc">The £299 Mystery Box ensures a minimum value of over £500 and includes a variety of items, including Hoodies, T-Shirts, Sweatpants, and accessories. Wardrobe staples, you can't go wrong. </h6>
+                    <small>Contains: 4 items</small>
+                    <h5 class="crimson-text">$400</h5>
+                    <small class="featuring">Featuring : off white, palm angels , amiri and heron preston</small>
+                    <button class="gradient__bg">SHOP NOW</button>
+                </div>
+
+                <div class="pills">
+                    <div class="mission__pill">
+                        <h6>AUTHENTICITY GUARANTEED</h6>
+                        <small>Partnered with 100+ leading luxury brands</small>
+                    </div>
+                    <div class="mission__pill">
+                        <h6>GUARANTEED VALUE</h6>
+                        <small>Partnered with 100+ leading luxury brands</small>
+                    </div>
+                    <div class="mission__pill">
+                        <h6>REFUNDS AND RETURNS ACCEPTED</h6>
+                        <small>Partnered with 100+ leading luxury brands</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
